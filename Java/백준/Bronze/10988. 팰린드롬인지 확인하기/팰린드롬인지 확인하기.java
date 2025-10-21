ae0efd3037cdbfdbd9a4eaee05780384e8308bd5
gyeonggu <lion989072@gmail.com>
2025-10-21 22:11:59 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 168 ms, Memory: 17660 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int answer = 1;
+
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
+                answer = 0;
+                break;
+            }
+        }
+        System.out.println(answer);
+    }
+}
